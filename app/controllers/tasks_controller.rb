546c8cd4cdd693fced000d0696d1cd7f7608ddfb
kenta.miyoshi <roial-ca.11@i.softbank.jp>
2019-08-06 09:13:34 +0000

    login out

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,29 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy,:update,:edit]
   
   def show
    
   end
 
   def index
-    @tasks=Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def create
-    @task=Task.new(task_params)
+    @task=current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] ='Taskは正常に追加されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger]='Taskが追加されませんでした'
-      render :new
+      render root_url
     end
   end
   
@@ -30,11 +35,12 @@ class TasksController < ApplicationController
   end
   
   def update
-
+   
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
     else
+      
       flash.now[:danger] = 'Taskは更新されませんでした'
       render :edit
     end
@@ -45,17 +51,24 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
-   private
+  private
    
-   def set_task
-     @task=Task.find(params[:id])
-   end
+  def set_task
+    @task=Task.find(params[:id])
+  end
 
   def task_params
    params.require(:task).permit(:content, :status)
   end 
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
  
 end
---
+++ db/migrate/20190806031929_adduser_reftotasks.rb
@@ -0,0 +1,4 @@
+class AdduserReftotasks < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
